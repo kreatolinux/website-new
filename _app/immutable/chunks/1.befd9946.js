6c761afcd83b7c566ba41b42de7b27623cb88c41
kreatoo <kreato@kreato.dev>
2023-04-16 20:07:53 +0000

    Deploying to gh-pages from @ kreatolinux/website-new@124a7759a2423608ea189179aa7e577c9a705b67 🚀

---
+++ _app/immutable/chunks/1.befd9946.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.848943ae.js";export{t as component};
--- _app/immutable/chunks/1.d0e10529.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.7d6d010f.js";export{t as component};
